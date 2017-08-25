621ccfa6cce164b8940423de203ca5c6eb2c979d
phong <phong@airbears2-10-142-140-25.airbears2.1918.berkeley.edu>
2017-08-24 17:54:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# getHW
+This bash script checks a given $HOME html page for any links (in the form of href tags) to homework files. Then, the script downloads these files to a specified folder. If there are any files in this folder that have the same name as the files that would be downloaded, these files are not downloaded.
+
+Only works for course websites that list all their homework in the same page. Mostly tested with Berkeley's CS70 Fall 2017 course website http://www.eecs70.org/, may work with other websites as well.
+
+Inspired by Josh Schreuder's BASH script to download NASA's picture of the day. Link: https://gist.github.com/JoshSchreuder/882666
+
+# TODO
+Add date checking to only check for new homeworks during days after they are released
+
+Only check once for every homework, no matter how many times there is a link
+
+Only check for the latest homework, rather than go through every homework
+
+Make the script do nothing or give an error message if there is a 404 error when downloading a file instead of making a bad file.
---
+++ gethw.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# ********************************
+# *** OPTIONS
+# ********************************
+# Set this to the directory you want files saved. End with / please
+SAVE_DIR=~/Documents/Coursework/
+# Set this to full homepage of course you want to download from. End with / please
+HOME=CHANGEME
+# Set this to the file type you want to grab TODO: add support for multiple file types
+FILE_TYPE=pdf
+# For people who know how to use bash regex, specify file pattern
+FILE_PAT="hw[[:digit:]]*\.$FILE_TYPE"
+# ********************************
+# *** Main
+# ********************************
+
+if [ $HOME == "CHANGEME" ]; then
+	echo "Please change default options."
+	exit
+fi
+
+# Download course page to find links to files
+wget $HOME -O /tmp/coursesite.html
+HW_REG="<a[[:blank:]]href=\"\($HOME\)\?\/\?\([[:alnum:]]\+\/\)\+$FILE_PAT\">"
+URL_REG="\/.*$FILE_TYPE"
+grep -n $HW_REG /tmp/coursesite.html
+SITE=$(grep -o $HW_REG /tmp/coursesite.html | grep -o $URL_REG)
+
+for URL in $SITE; do
+	FILE_NAME=$(echo $URL | grep -o $FILE_PAT)
+	#echo $FILE_NAME
+	if [ -f $SAVE_DIR$FILE_NAME ]; then
+		echo "$FILE_NAME exists"
+	else
+		wget $HOME$URL -O $SAVE_DIR$FILE_NAME
+	fi
+done
+
+# Cleanup downloaded course page
+if [ -e "/tmp/coursesite.html" ]; then
+    rm /tmp/coursesite.html
+fi
